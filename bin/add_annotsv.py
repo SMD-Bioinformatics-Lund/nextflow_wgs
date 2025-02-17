ac274140bc1c887fc3269e67cf442cc95f330ecc
Viktor Henmyr <viktor_henmyr@hotmail.com>
2025-02-17 13:19:30 +0100

    type hints and some logic changes for file handling

--- bin/add_annotsv.py
+++ bin/add_annotsv.py
@@ -5,34 +5,34 @@ from pprint import pprint
 import cmdvcf
 import argparse
 
-def main(args):
+def main(args: object):
     """
     main function, reads VCF line by line and adds all AnnotSV annotations
     Updates vcf-header with the new annotations being added
     """
     annotsv_variants, annotsv_headers = read_annotsv_vcf(args.annotsv)
     vcf_object = VariantFile(args.input_vcf)
-    
-    vcf_header = join_headers(vcf_object.header,annotsv_headers)
+    original_header = vcf_object.header
+    vcf_header = join_headers(original_header,annotsv_headers)
     vcf_out = VariantFile(args.out_vcf, "w", header=vcf_header)
     vcf_out.close()
-    for var in vcf_object.fetch():
-        # to fix END flag disapearing from pysam object
-        # pysam eats up END from original, don't know why. (PERL vfc2.pm <3)
-        var_dict = cmdvcf.parse_variant(var,vcf_object.header)
-        var_dict['INFO']['END'] = var.stop
-        svtype = var.info['SVTYPE']
-        # AnnotSV treats TDUP as DUP
-        if svtype == "TDUP":
-            svtype = "DUP"
-        simple_id = f"{var.chrom}_{var.pos}_{svtype}"
-        if simple_id in annotsv_variants:
-            var_dict['INFO'] = add_annotsv_annotations(var_dict['INFO'],annotsv_variants[simple_id]['INFO'])
-        vcf_str = cmdvcf.print_vcf_record(var_dict,vcf_object.header)
-        with open(args.out_vcf, "a") as vcf_out:
+    with open(args.out_vcf, "a") as vcf_out:
+        for var in vcf_object.fetch():
+            # to fix END flag disapearing from pysam object
+            # pysam eats up END from original, don't know why. (PERL vfc2.pm <3)
+            var_dict = cmdvcf.parse_variant(var,vcf_object.header)
+            var_dict['INFO']['END'] = var.stop
+            svtype = var.info['SVTYPE']
+            # AnnotSV treats TDUP as DUP
+            if svtype == "TDUP":
+                svtype = "DUP"
+            simple_id = f"{var.chrom}_{var.pos}_{svtype}"
+            if simple_id in annotsv_variants:
+                var_dict['INFO'] = add_annotsv_annotations(var_dict['INFO'],annotsv_variants[simple_id]['INFO'])
+            vcf_str = cmdvcf.vcf_string(var_dict,vcf_object.header)
             vcf_out.write(vcf_str)
-        
-def add_annotsv_annotations(original_info,annotation):
+            
+def add_annotsv_annotations(original_info: dict,annotation: dict):
     """
     Add the unique(new) keys from annotsv to original VCF INFO
     """
@@ -41,7 +41,7 @@ def add_annotsv_annotations(original_info,annotation):
             original_info[key] = annotation[key]
     return original_info
 
-def join_headers(original_header, new_annotations):
+def join_headers(original_header: object, new_annotations: dict):
     """
     add new annotation headers to INFO
     """
@@ -55,12 +55,12 @@ def join_headers(original_header, new_annotations):
             )
     return original_header
 
-def read_annotsv_vcf(file_path):
+def read_annotsv_vcf(file_path: str):
     """
     read VCF from annotsv
     """
-    vcf_in = VariantFile(file_path, "r")
     annotsv_variants = {}
+    vcf_in = VariantFile(file_path, "r")
     for var in vcf_in:
         var_dict = cmdvcf.parse_variant(var,vcf_in.header)
         # these are not needed, AnnotSV saves input VCF info-field and stores them weirdly
@@ -69,11 +69,11 @@ def read_annotsv_vcf(file_path):
         simple_id_parts = var_dict['INFO']['AnnotSV_ID'].split("_")
         simple_id = f"{simple_id_parts[0]}_{simple_id_parts[1]}_{simple_id_parts[3]}"
         annotsv_variants[simple_id] = var_dict
-
-    annotsv_headers = get_relevant_headers(vcf_in)
+    annotsv_headers = remove_cohort_headers(vcf_in)
+    vcf_in.close()
     return annotsv_variants,annotsv_headers
 
-def get_relevant_headers(vcf_in):
+def remove_cohort_headers(vcf_in: object):
     """
     get VCF headers of info-field that gets added from AnnotSV
 
@@ -81,9 +81,7 @@ def get_relevant_headers(vcf_in):
     """
     headers = []
     for key, record in vcf_in.header.info.items():
-        if 'ample' in key or 'cohort' in key:
-            ...
-        else:
+        if key not in ['ample', 'cohort']:
             info_dict = {}
             info_dict['key'] = key
             info_dict['description'] = record.description
@@ -92,7 +90,7 @@ def get_relevant_headers(vcf_in):
             headers.append(info_dict)
     return headers
 
-if __name__ == "__main__":
+def parse_arguments():
     parser = argparse.ArgumentParser(description="Adds AnnotSV annotations to a VCF. Uses AnnotSV tsv that has been converted to VCF via variantconvert")
 
     parser.add_argument(
@@ -117,4 +115,8 @@ if __name__ == "__main__":
         help="name of output.vcf"
     )
     args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_arguments()
     main(args)
