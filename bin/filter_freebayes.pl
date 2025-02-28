74bc17ebb6cfca54fa8c27a699fa33ca23b4c92f
Alexander Koc <alexander.koc@skane.se>
2025-02-28 10:54:52 +0100

    don't exclude header in filter_bayes

--- bin/filter_freebayes.pl
+++ bin/filter_freebayes.pl
@@ -2,37 +2,40 @@
 use strict;
 use Getopt::Long;
 use File::Basename;
-use lib dirname (__FILE__);
+use lib dirname(__FILE__);
 use vcf2;
 use strict;
 use Data::Dumper;
 
+my $vcf = CMD::vcf2->new( 'file' => $ARGV[0] );
 
-
-my $vcf = CMD::vcf2->new('file'=>$ARGV[0] );
-
-#print $vcf->{header_str};
+print $vcf->{header_str};
 while ( my $a = $vcf->next_var() ) {
 
-    if ($a->{INFO}->{CLNSIG}) {
+    if ( $a->{INFO}->{CLNSIG} ) {
+
         #print Dumper($a);
-            if ($a->{GT}->[0]->{GT} eq '0/0') {
-                next;
-            }
-        if ($a->{INFO}->{CLNSIG} eq 'Pathogenic' || $a->{INFO}->{CLNSIG} eq 'Likely_pathogenic' || $a->{INFO}->{CLNSIG} eq 'Pathogenic/Likely_pathogenic' ) {
-            my @str = split/\t/,$a->{vcf_str};
-            print join("\t",@str[0..6])."\t";
-            print "AC=".$a->{INFO}->{AC};
-            print ";AF=".$a->{INFO}->{AF};
-            print ";DP=".$a->{INFO}->{DP};
-            print ";MQ=".$a->{INFO}->{MQM};
+        if ( $a->{GT}->[0]->{GT} eq '0/0' ) {
+            next;
+        }
+        if (   $a->{INFO}->{CLNSIG} eq 'Pathogenic'
+            || $a->{INFO}->{CLNSIG} eq 'Likely_pathogenic'
+            || $a->{INFO}->{CLNSIG} eq 'Pathogenic/Likely_pathogenic' )
+        {
+            my @str = split /\t/, $a->{vcf_str};
+            print join( "\t", @str[ 0 .. 6 ] ) . "\t";
+            print "AC=" . $a->{INFO}->{AC};
+            print ";AF=" . $a->{INFO}->{AF};
+            print ";DP=" . $a->{INFO}->{DP};
+            print ";MQ=" . $a->{INFO}->{MQM};
 
             print "\t";
 
             print "GT:AD:DP:GQ:PGT:PID:PL\t";
-            
-            print $a->{GT}->[0]->{GT}.":"."100,100".":"."200".":"."0".":"."0".":"."0".":"."0";
 
+            print $a->{GT}->[0]->{GT} . ":"
+              . "100,100" . ":" . "200" . ":" . "0" . ":" . "0" . ":" . "0"
+              . ":" . "0";
 
             print "\n";
         }
@@ -41,8 +44,6 @@ while ( my $a = $vcf->next_var() ) {
     else {
         next;
     }
-    
-    
 
 }
 
