78e4b1573ff3fb319d6f75b1dab9685a0c7adee3
Jakob Willforss <jakob.willforss@hotmail.com>
2025-06-12 16:41:24 +0200

    Resolve ROH issues

--- bin/gens_sample_track.py
+++ bin/gens_sample_track.py
@@ -37,12 +37,12 @@ class RohEntry:
         self.chrom = fields[2]
         self.start = int(fields[3])
         self.end = int(fields[4])
-        self.qual = float(fields[5])
+        self.length = float(fields[5])
+        self.nbr_markers = float(fields[6])
+        self.quality = float(fields[7])
 
     def get_length(self) -> int:
-        # FIXME: Keeping it in line with previous
         return self.end - self.start
-        # return self.end - self.start + 1
 
     def get_bed_fields(self) -> list[str]:
         return [self.chrom, str(self.start), str(self.end), "ROH", ".", ".", ".", ".", TOP_COLOR]
@@ -65,7 +65,6 @@ class UPDEntry:
 
     def get_length(self) -> int:
         return self.end - self.start
-        # return self.end - self.start + 1
 
     def get_bed_fields(self) -> list[str]:
         return [self.chrom, str(self.start), str(self.end), "UPD", ".", ".", ".", ".", BOTTOM_COLOR]
@@ -81,7 +80,7 @@ class CovEntry:
         self.cn = float(fields[3])
 
     def get_length(self) -> int:
-        return self.end - self.start + 1
+        return self.end - self.start
 
 
 class ChromCovEntry:
@@ -100,6 +99,7 @@ def main(
     cov_path: Path,
     chrom_length_path: Path,
     sample: str,
+    sex: str,
     roh_quality_threshold: int,
     min_length: int,
     cov_diff_threshold: int,
@@ -111,13 +111,19 @@ def main(
     total_chrom_length = sum(chrom_lengths.values())
     roh_entries: list[RohEntry] = parse_roh(roh_path, sample, roh_quality_threshold)
     upd_entries: list[UPDEntry] = parse_upd(upd_path)
-    # avg_cov_entries: list[ChromCovEntry] = parse_cov(cov_path, cov_diff_threshold)
+    avg_cov_entries: list[ChromCovEntry] = parse_cov(cov_path, cov_diff_threshold, sex)
+
+    print("Number ROH entries", len(roh_entries))
 
     tot_roh_length = sum(
-        [entry.get_length() for entry in roh_entries if entry.chrom not in SEX_CHROMS]
+        [
+            entry.get_length()
+            for entry in roh_entries
+            if entry.chrom not in SEX_CHROMS and entry.chrom in CHROMS
+        ]
     )
-    print(tot_roh_length)
-    print(total_chrom_length)
+    print("ROH tot length", tot_roh_length)
+    print("Tot chrom length", total_chrom_length)
     roh_perc = float(tot_roh_length) / float(total_chrom_length) * 100
 
     with open_file(output_upd_roh, "w") as out_fh:
@@ -128,17 +134,15 @@ def main(
             if entry.get_length() > min_length:
                 print("\t".join(entry.get_bed_fields()), file=out_fh)
 
-    # with open_file(output_chr_cov, "w") as out_fh:
-    #     print("\t".join(["row_name", "type", "value", "color"]), file=out_fh)
-    #     for chrom_cov in avg_cov_entries:
-    #         label = "Estimated chromosomal copy numbers"
-    #         print(f"{chrom_cov.chrom}\t{label}\t{chrom_cov.cov}\t{chrom_cov.color}", file=out_fh)
+    with open_file(output_chr_cov, "w") as out_fh:
+        print("\t".join(["row_name", "type", "value", "color"]), file=out_fh)
+        for chrom_cov in avg_cov_entries:
+            label = "Estimated chromosomal copy numbers"
+            print(f"{chrom_cov.chrom}\t{label}\t{chrom_cov.cov}\t{chrom_cov.color}", file=out_fh)
 
     with open_file(output_meta, "w") as out_fh:
         print("\t".join(["type", "value"]), file=out_fh)
         print(roh_perc)
-        # print("\t".join(["%ROH", str(roh_perc)]))
-        # print("\t".join(["%ROH", str(roh_perc)]), file=out_fh)
 
 
 def parse_chrom_lengths(chrom_lengths_path: Path) -> dict[str, int]:
@@ -151,12 +155,12 @@ def parse_chrom_lengths(chrom_lengths_path: Path) -> dict[str, int]:
             fields = line.split("\t")
             chrom = fields[1].split(":")[1]
             length = fields[2].split(":")[1]
-            if chrom not in SEX_CHROMS:
+            if chrom not in SEX_CHROMS and chrom in CHROMS:
                 chrom_lenghts[chrom] = int(length)
     return chrom_lenghts
 
 
-def parse_cov(cov: Path, cov_diff_thres: float) -> list[ChromCovEntry]:
+def parse_cov(cov: Path, cov_diff_thres: float, sex: str) -> list[ChromCovEntry]:
     cov_sums = {}
     with open_file(cov, "r") as cov_fh:
         for line in cov_fh:
@@ -179,7 +183,7 @@ def parse_cov(cov: Path, cov_diff_thres: float) -> list[ChromCovEntry]:
     for chrom in CHROMS:
 
         color = "rgb(0,0,0)"
-        chrom_count = 1 if chrom in sex_chroms else 2
+        chrom_count = 1 if sex == "M" and chrom in sex_chroms else 2
         # Calculate the full value
         cn_val = chrom_count * 2 ** avg_covs[chrom]
         if abs(cn_val - chrom_count) > cov_diff_thres:
@@ -198,8 +202,12 @@ def parse_roh(roh_path: Path, sample: str, qual_thres: float) -> list[RohEntry]:
             if line.startswith("#") or line.startswith("ST"):
                 continue
             line = line.rstrip()
+            fields = line.split("\t")
+            # There is no quality information available
+            if len(fields) < 7:
+                continue
             roh_entry = RohEntry(line)
-            if roh_entry.sample == sample and roh_entry.qual > qual_thres:
+            if roh_entry.sample == sample and roh_entry.quality > qual_thres:
                 roh_entries_per_chrom.append(roh_entry)
     return roh_entries_per_chrom
 
@@ -225,6 +233,7 @@ def parse_arguments():
     parser.add_argument("--chrom_lengths", required=True, type=Path)
 
     parser.add_argument("--sample", required=True, type=str)
+    parser.add_argument("--sex", required=True, type=str)
 
     parser.add_argument("--roh_quality_threshold", default=85, type=int)
     parser.add_argument("--min_length", default=100000, type=int)
@@ -235,6 +244,10 @@ def parse_arguments():
     parser.add_argument("--output_meta", type=Path)
 
     args = parser.parse_args()
+
+    if not args.sex in {"M", "F"}:
+        raise ValueError("--sex must be either 'M' or 'F'")
+
     return args
 
 
@@ -246,6 +259,7 @@ if __name__ == "__main__":
         args.cov,
         args.chrom_lengths,
         args.sample,
+        args.sex,
         args.roh_quality_threshold,
         args.min_length,
         args.cov_diff_threshold,
