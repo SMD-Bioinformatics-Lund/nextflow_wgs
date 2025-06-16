20871bd99d400fe217fd259a4afdcd13a26f2951
Jakob Willforss <jakob.willforss@hotmail.com>
2025-06-16 10:01:32 +0200

    Draft include extended UPD values in parsing

--- bin/gens_sample_track.py
+++ bin/gens_sample_track.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from collections import defaultdict
 import gzip
 from pathlib import Path
 from typing import Literal, TextIO
@@ -93,9 +94,18 @@ class ChromCovEntry:
         return [self.chrom, str(self.cov), self.color]
 
 
+class UPDSite:
+    def __init__(self, chrom: str, start: int, end: int, type: str):
+        self.chrom = chrom
+        self.start = start
+        self.end = end
+        self.type = type
+
+
 def main(
     roh_path: Path,
     upd_path: Path,
+    upd_sites_path: Path,
     cov_path: Path,
     chrom_length_path: Path,
     sample: str,
@@ -110,6 +120,7 @@ def main(
     total_chrom_length = sum(chrom_lengths.values())
     roh_entries: list[RohEntry] = parse_roh(roh_path, sample, roh_quality_threshold)
     upd_entries: list[UPDEntry] = parse_upd(upd_path)
+    upd_site_info: dict[str, dict[str, str]] = parse_upd_sites(upd_sites_path)
     avg_cov_entries: list[ChromCovEntry] = parse_cov(cov_path, cov_diff_threshold, sex)
 
     print("Number ROH entries", len(roh_entries))
@@ -132,14 +143,78 @@ def main(
             print("\t".join(entry.get_bed_fields()), file=out_fh)
 
     with open_file(output_chr_cov, "w") as out_fh:
-        print("\t".join(["row_name", "type", "value", "color"]), file=out_fh)
+
+        print("\t".join(["Chromosome", "type", "value", "color"]), file=out_fh)
         for chrom_cov in avg_cov_entries:
             label = "Estimated chromosomal copy numbers"
             print(f"{chrom_cov.chrom}\t{label}\t{chrom_cov.cov}\t{chrom_cov.color}", file=out_fh)
 
+        upd_labels = [
+            "Chromosome",
+            "Total SNPs",
+            "Non-informative",
+            "Mismatch father",
+            "Mismatch mother",
+            "Anti-UPD",
+        ]
+        for chrom in CHROMS:
+            for upd_label in upd_labels:
+                field_value = upd_site_info[chrom][upd_label]
+                print(f"{chrom}\t{upd_label}\t{field_value}\trgb(0,0,0)", file=out_fh)
+
     with open_file(output_meta, "w") as out_fh:
         print("\t".join(["type", "value"]), file=out_fh)
-        print(roh_perc)
+        print("\t".join(["%ROH", str(roh_perc)]), file=out_fh)
+
+
+def parse_upd_sites(upd_sites: Path) -> dict[str, dict[str, str]]:
+
+    sum_per_chrom: dict[str, int] = {}
+    sum_chrom_type: dict[str, dict[str, int]] = {}
+
+    for chrom in CHROMS:
+        sum_per_chrom[chrom] = 0
+        sum_chrom_type[chrom] = {}
+
+    with open_file(upd_sites, "r") as in_fh:
+        for line in in_fh:
+            fields = line.split("\t")
+            chrom = fields[0]
+            upd_type = fields[3]
+
+            sum_per_chrom[chrom] += 1
+
+            if sum_chrom_type[chrom].get(upd_type) is None:
+                sum_chrom_type[chrom][upd_type] = 0
+
+            sum_chrom_type[chrom][upd_type] += 1
+
+    out_fields: dict[str, dict[str, str]] = defaultdict(dict)
+    for chrom in CHROMS:
+        chrom_tot = sum_per_chrom[chrom]
+        chrom_types: dict[str, int] = sum_chrom_type[chrom]
+
+        paternal = chrom_types["UPD_PATERNAL_ORIGIN"]
+        paternal_perc = round(100 * paternal / chrom_tot, 1)
+        maternal = chrom_types["UPD_MATERNAL_ORIGIN"]
+        maternal_perc = round(100 * maternal / chrom_tot, 1)
+        anti = chrom_types["ANTI_UPD"]
+        anti_perc = round(100 * anti / chrom_tot, 1)
+        non_informative = paternal + maternal + anti
+        non_informative_perc = round(100 * non_informative / chrom_tot, 1)
+
+        chrom_info: dict[str, str] = {
+            "Chromosome": chrom,
+            "Total SNPs": str(chrom_tot),
+            "Non-informative": f"{non_informative} ({non_informative_perc}%)",
+            "Mismatch father": f"{paternal} ({paternal_perc}%)",
+            "Mismatch mother": f"{maternal} ({maternal_perc}%)",
+            "Anti-UPD": f"{anti} ({anti_perc}%)"
+        }
+
+        out_fields[chrom] = chrom_info
+
+    return out_fields
 
 
 def parse_chrom_lengths(chrom_lengths_path: Path) -> dict[str, int]:
@@ -226,6 +301,7 @@ def parse_arguments():
 
     parser.add_argument("--roh", required=True, type=Path)
     parser.add_argument("--upd", required=True, type=Path)
+    parser.add_argument("--upd_sites", required=True, type=Path)
     parser.add_argument("--cov", required=True, type=Path)
     parser.add_argument("--chrom_lengths", required=True, type=Path)
 
@@ -252,6 +328,7 @@ if __name__ == "__main__":
     main(
         args.roh,
         args.upd,
+        args.upd_sites,
         args.cov,
         args.chrom_lengths,
         args.sample,
