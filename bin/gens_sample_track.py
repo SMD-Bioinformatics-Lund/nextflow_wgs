2270e59cbf6cffb896a2c21e6f9f845d60b09de9
Jakob Willforss <jakob.willforss@hotmail.com>
2025-06-16 13:42:03 +0200

    Cleanup

--- bin/gens_sample_track.py
+++ bin/gens_sample_track.py
@@ -4,105 +4,27 @@ import argparse
 from collections import defaultdict
 import gzip
 from pathlib import Path
-from typing import Literal, TextIO
+from typing import Dict, List, Literal, TextIO
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 LOG = logging.getLogger(__name__)
 
 description = """
-Parse ROH output and UPD output into a single bed file
-This bed will then be loaded into Gens as a sample annotation track
+Generates inputs for Gens v4+
+
+* Parses ROH output and UPD output into a single bed file
+* Calculates overal ROH %
+* Calculates average chromosome coverage
+* Summarizes various UPD / ROH metrics
 """
 
-# FIXME: Is this the UPD?
-TOP_COLOR = "rgba(255,186,60)"
-BOTTOM_COLOR = "rgba(255,75,75)"
+ROH_COLOR = "rgb(255,186,60)"
+UPD_COLOR = "rgb(255,75,75)"
 
-CHROMS = [str(i) for i in range(1, 23)] + ["X", "Y"]
+AUTO_CHROMS = [str(i) for i in range(1, 23)]
 SEX_CHROMS = ["X", "Y"]
-
-
-def open_file(path: Path, read_or_write: Literal["r", "w"]) -> TextIO:
-    if path.suffix == ".gz":
-        if read_or_write == "r":
-            return gzip.open(path, "rt")
-        else:
-            return gzip.open(path, "wt")
-    else:
-        return path.open(read_or_write)
-
-
-class RohEntry:
-    def __init__(self, line):
-        line = line.rstrip()
-        fields = line.split("\t")
-        self.sample = fields[1]
-        self.chrom = fields[2]
-        self.start = int(fields[3])
-        self.end = int(fields[4])
-        self.length = float(fields[5])
-        self.nbr_markers = float(fields[6])
-        self.quality = float(fields[7])
-
-    def get_length(self) -> int:
-        return self.end - self.start
-
-    def get_bed_fields(self) -> list[str]:
-        return [self.chrom, str(self.start), str(self.end), "ROH", ".", ".", ".", ".", TOP_COLOR]
-
-
-class UPDEntry:
-    def __init__(self, line):
-        line = line.rstrip()
-        fields = line.split("\t")
-        self.chrom = fields[0]
-        self.start = int(fields[1])
-        self.end = int(fields[2])
-        details_str = fields[3]
-
-        details = {}
-        for entry in details_str.split(";"):
-            key, value = entry.split("=")
-            details[key] = value
-        self.details = details
-
-    def get_length(self) -> int:
-        return self.end - self.start
-
-    def get_bed_fields(self) -> list[str]:
-        return [self.chrom, str(self.start), str(self.end), "UPD", ".", ".", ".", ".", BOTTOM_COLOR]
-
-
-class CovEntry:
-    def __init__(self, line):
-        line = line.rstrip()
-        fields = line.split("\t")
-        self.chrom = fields[0]
-        self.start = int(fields[1])
-        self.end = int(fields[2])
-        self.cn = float(fields[3])
-
-    def get_length(self) -> int:
-        return self.end - self.start
-
-
-class ChromCovEntry:
-    def __init__(self, chrom: str, cov: float, color: str):
-        self.chrom = chrom
-        self.cov = cov
-        self.color = color
-
-    def get_fields(self) -> list[str]:
-        return [self.chrom, str(self.cov), self.color]
-
-
-class UPDSite:
-    def __init__(self, chrom: str, start: int, end: int, type: str):
-        self.chrom = chrom
-        self.start = start
-        self.end = end
-        self.type = type
+CHROMS = AUTO_CHROMS + SEX_CHROMS
 
 
 def main(
@@ -125,25 +47,21 @@ def main(
     chrom_lengths: dict[str, int] = parse_chrom_lengths(chrom_length_path)
     total_chrom_length = sum(chrom_lengths.values())
     LOG.info("Parsing ROH")
-    roh_entries: list[RohEntry] = parse_roh(roh_path, sample, roh_quality_threshold)
+    roh_entries: List[RohEntry] = parse_roh(roh_path, sample, roh_quality_threshold)
     LOG.info("Parsing UPD")
-    upd_entries: list[UPDEntry] = parse_upd(upd_path)
+    upd_entries: List[UPDEntry] = parse_upd(upd_path)
     LOG.info("Parsing UPD sites")
-    upd_site_info: dict[str, dict[str, str]] = parse_upd_sites(upd_sites_path)
+    upd_site_info: Dict[str, dict[str, str]] = parse_upd_sites(upd_sites_path)
     LOG.info("Parsing coverage")
-    avg_cov_entries: list[ChromCovEntry] = parse_cov(cov_path, cov_diff_threshold, sex)
-
-    # LOG.info("Number ROH entries", len(roh_entries))
+    avg_cov_entries: List[ChromCovEntry] = parse_cov(cov_path, cov_diff_threshold, sex)
 
     tot_roh_length = sum(
         [
             entry.get_length()
             for entry in roh_entries
-            if entry.chrom not in SEX_CHROMS and entry.chrom in CHROMS
+            if entry.chrom in AUTO_CHROMS
         ]
     )
-    # print("ROH tot length", tot_roh_length)
-    # print("Tot chrom length", total_chrom_length)
     roh_perc = float(tot_roh_length) / float(total_chrom_length) * 100
 
     LOG.info("Writing global meta")
@@ -162,7 +80,7 @@ def main(
             print(f"{chrom_cov.chrom}\t{label}\t{chrom_cov.cov}\t{chrom_cov.color}", file=out_fh)
 
         upd_labels = [
-            "Chromosome",
+            # "Chromosome",
             "Total SNPs",
             "Non-informative",
             "Mismatch father",
@@ -177,6 +95,78 @@ def main(
     with open_file(output_meta, "w") as out_fh:
         print("\t".join(["type", "value"]), file=out_fh)
         print("\t".join(["%ROH", str(roh_perc)]), file=out_fh)
+
+
+class RohEntry:
+    def __init__(self, line):
+        line = line.rstrip()
+        fields = line.split("\t")
+        self.sample = fields[1]
+        self.chrom = fields[2]
+        self.start = int(fields[3])
+        self.end = int(fields[4])
+        self.length = float(fields[5])
+        self.nbr_markers = float(fields[6])
+        self.quality = float(fields[7])
+
+    def get_length(self) -> int:
+        return self.end - self.start
+
+    def get_bed_fields(self) -> List[str]:
+        return [self.chrom, str(self.start), str(self.end), "ROH", ".", ".", ".", ".", ROH_COLOR]
+
+
+class UPDEntry:
+    def __init__(self, line):
+        line = line.rstrip()
+        fields = line.split("\t")
+        self.chrom = fields[0]
+        self.start = int(fields[1])
+        self.end = int(fields[2])
+        details_str = fields[3]
+
+        details = {}
+        for entry in details_str.split(";"):
+            key, value = entry.split("=")
+            details[key] = value
+        self.details = details
+
+    def get_length(self) -> int:
+        return self.end - self.start
+
+    def get_bed_fields(self) -> List[str]:
+        return [self.chrom, str(self.start), str(self.end), "UPD", ".", ".", ".", ".", UPD_COLOR]
+
+
+class CovEntry:
+    def __init__(self, line):
+        line = line.rstrip()
+        fields = line.split("\t")
+        self.chrom = fields[0]
+        self.start = int(fields[1])
+        self.end = int(fields[2])
+        self.cn = float(fields[3])
+
+    def get_length(self) -> int:
+        return self.end - self.start
+
+
+class ChromCovEntry:
+    def __init__(self, chrom: str, cov: float, color: str):
+        self.chrom = chrom
+        self.cov = cov
+        self.color = color
+
+    def get_fields(self) -> List[str]:
+        return [self.chrom, str(self.cov), self.color]
+
+
+class UPDSite:
+    def __init__(self, chrom: str, start: int, end: int, type: str):
+        self.chrom = chrom
+        self.start = start
+        self.end = end
+        self.type = type
 
 
 def parse_upd_sites(upd_sites: Path) -> dict[str, dict[str, str]]:
@@ -204,10 +194,8 @@ def parse_upd_sites(upd_sites: Path) -> dict[str, dict[str, str]]:
 
     out_fields: dict[str, dict[str, str]] = defaultdict(dict)
     for chrom in CHROMS:
-        chrom_tot = sum_per_chrom[chrom] 
+        chrom_tot = sum_per_chrom[chrom]
         chrom_types: dict[str, int] = sum_chrom_type[chrom]
-
-        print(chrom_types)
 
         paternal = chrom_types.get("UPD_PATERNAL_ORIGIN") or 0
         paternal_perc = round(100 * paternal / chrom_tot, 1) if chrom_tot > 0 else 0
@@ -219,12 +207,11 @@ def parse_upd_sites(upd_sites: Path) -> dict[str, dict[str, str]]:
         non_informative_perc = round(100 * non_informative / chrom_tot, 1) if chrom_tot > 0 else 0
 
         chrom_info: dict[str, str] = {
-            "Chromosome": chrom,
             "Total SNPs": str(chrom_tot),
             "Non-informative": f"{non_informative} ({non_informative_perc}%)",
             "Mismatch father": f"{paternal} ({paternal_perc}%)",
             "Mismatch mother": f"{maternal} ({maternal_perc}%)",
-            "Anti-UPD": f"{anti} ({anti_perc}%)"
+            "Anti-UPD": f"{anti} ({anti_perc}%)",
         }
 
         out_fields[chrom] = chrom_info
@@ -242,12 +229,12 @@ def parse_chrom_lengths(chrom_lengths_path: Path) -> dict[str, int]:
             fields = line.split("\t")
             chrom = fields[1].split(":")[1]
             length = fields[2].split(":")[1]
-            if chrom not in SEX_CHROMS and chrom in CHROMS:
+            if chrom in AUTO_CHROMS:
                 chrom_lenghts[chrom] = int(length)
     return chrom_lenghts
 
 
-def parse_cov(cov: Path, cov_diff_thres: float, sex: str) -> list[ChromCovEntry]:
+def parse_cov(cov: Path, cov_diff_thres: float, sex: str) -> List[ChromCovEntry]:
     cov_sums = {}
     with open_file(cov, "r") as cov_fh:
         for line in cov_fh:
@@ -264,26 +251,24 @@ def parse_cov(cov: Path, cov_diff_thres: float, sex: str) -> list[ChromCovEntry]
     for chrom, sums in cov_sums.items():
         avg_covs[chrom] = sums["acc"] / sums["nval"]
 
-    sex_chroms = ["X", "Y"]
-
     scaled_covs = []
     for chrom in CHROMS:
 
         color = "rgb(0,0,0)"
-        chrom_count = 1 if sex == "M" and chrom in sex_chroms else 2
+        chrom_count = 1 if sex == "M" and chrom in SEX_CHROMS else 2
         # Calculate the full value
         cn_val = chrom_count * 2 ** avg_covs[chrom]
         if abs(cn_val - chrom_count) > cov_diff_thres:
             color = "rgb(255,0,0)"
 
-        scaled_covs.append(ChromCovEntry(chrom, cn_val, color))
+        scaled_covs.append(ChromCovEntry(chrom, round(cn_val, 2), color))
 
     return scaled_covs
 
 
-def parse_roh(roh_path: Path, sample: str, qual_thres: float) -> list[RohEntry]:
+def parse_roh(roh_path: Path, sample: str, qual_thres: float) -> List[RohEntry]:
 
-    roh_entries_per_chrom: list[RohEntry] = []
+    roh_entries_per_chrom: List[RohEntry] = []
     with open_file(roh_path, "r") as roh_fh:
         for line in roh_fh:
             if line.startswith("#") or line.startswith("ST"):
@@ -299,9 +284,9 @@ def parse_roh(roh_path: Path, sample: str, qual_thres: float) -> list[RohEntry]:
     return roh_entries_per_chrom
 
 
-def parse_upd(upd: Path) -> list[UPDEntry]:
+def parse_upd(upd: Path) -> List[UPDEntry]:
 
-    upd_entries: list[UPDEntry] = []
+    upd_entries: List[UPDEntry] = []
     with open_file(upd, "r") as upd_fh:
         for line in upd_fh:
             upd_entry = UPDEntry(line)
@@ -309,26 +294,85 @@ def parse_upd(upd: Path) -> list[UPDEntry]:
     return upd_entries
 
 
+def open_file(path: Path, read_or_write: Literal["r", "w"]) -> TextIO:
+    if path.suffix == ".gz":
+        if read_or_write == "r":
+            return gzip.open(path, "rt")
+        else:
+            return gzip.open(path, "wt")
+    else:
+        return path.open(read_or_write)
+
+
 def parse_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=description)
 
-    # FIXME: Understand the difference roh / upd
+    parser.add_argument(
+        "--roh", required=True, type=Path, help="ROH output as provided by the bcftools command roh"
+    )
+    parser.add_argument(
+        "--upd_regions",
+        required=True,
+        type=Path,
+        help="UPD regions output from: https://github.com/bjhall/upd",
+    )
+    parser.add_argument(
+        "--upd_sites",
+        required=True,
+        type=Path,
+        help="UPD sites output from: https://github.com/bjhall/upd",
+    )
+    parser.add_argument(
+        "--cov",
+        required=True,
+        type=Path,
+        help="Copy ratio output from GATK's CollectReadCounts+DenoiseReadCounts",
+    )
+    parser.add_argument(
+        "--chrom_lengths",
+        required=True,
+        type=Path,
+        help=".dict file generated by calculating FASTA sequences lengths using samtools faidx and Picard's CreateSequenceDictionary",
+    )
 
-    parser.add_argument("--roh", required=True, type=Path)
-    parser.add_argument("--upd", required=True, type=Path)
-    parser.add_argument("--upd_sites", required=True, type=Path)
-    parser.add_argument("--cov", required=True, type=Path)
-    parser.add_argument("--chrom_lengths", required=True, type=Path)
+    parser.add_argument(
+        "--sample",
+        required=True,
+        type=str,
+        help="Sample ID used to filter out proband-only rows from ROH input",
+    )
+    parser.add_argument(
+        "--sex",
+        required=True,
+        type=str,
+        help="Sex information (M / F) used to scale copy ratio to chromosome level for sex chromosomes",
+    )
 
-    parser.add_argument("--sample", required=True, type=str)
-    parser.add_argument("--sex", required=True, type=str)
+    parser.add_argument(
+        "--roh_quality_threshold",
+        default=85,
+        type=int,
+        help="Only entries with quality threshold above this in --roh are considered",
+    )
+    parser.add_argument(
+        "--cov_diff_threshold",
+        default=0.1,
+        type=int,
+        help="Chromosome coverage values differing more than this are colored in red in output data",
+    )
 
-    parser.add_argument("--roh_quality_threshold", default=85, type=int)
-    parser.add_argument("--cov_diff_threshold", default=10, type=int)
-
-    parser.add_argument("--output_upd_roh", type=Path)
-    parser.add_argument("--output_chr_cov", type=Path)
-    parser.add_argument("--output_meta", type=Path)
+    parser.add_argument(
+        "--output_upd_roh", required=True, type=Path, help="Bed ranges with UPD / ROH information"
+    )
+    parser.add_argument(
+        "--output_per_chrom",
+        required=True,
+        type=Path,
+        help="Writes per-chromosome information about coverage and UPD details",
+    )
+    parser.add_argument(
+        "--output_meta", required=True, type=Path, help="Output global meta info (i.e. ROH%)"
+    )
 
     args = parser.parse_args()
 
