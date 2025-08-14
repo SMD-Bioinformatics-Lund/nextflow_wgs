517ec1d79ce8bce0ad18b554b168ed4413267b8a
Jakob Willforss <jakob.willforss@hotmail.com>
2025-08-14 14:09:18 +0200

    Cleanup and Python 3.12 container

--- bin/genotype_to_json.py
+++ bin/genotype_to_json.py
@@ -2,26 +2,12 @@
 
 import json
 import argparse
-
-
-def parse_genotype_file(genotype_file_path):
-    genotype_dict = {}
-    with open(genotype_file_path, 'r') as genotype_file:
-        for line in genotype_file:
-            if line.startswith("#"):
-                continue
-            parts = line.split()
-            # chrom = parts[0]
-            var_id = parts[2]
-            sample_genotype = parts[9]
-            genotype = sample_genotype.split(":")[0]
-            genotype_dict[var_id] = genotype
-    return genotype_dict
+from typing import Dict
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Convert genotype data file into a JSON blob.")
-    parser.add_argument('genotype_file', type=str, help="Path to the genotype file")
+    parser = argparse.ArgumentParser(description="Convert VCF genotypes into a JSON file.")
+    parser.add_argument('genotype_file', type=str, help="Path to the VCF file")
     parser.add_argument('output_file', type=str, help="Path to save the output JSON file")
     
     args = parser.parse_args()
@@ -31,6 +17,20 @@ def main():
     with open(args.output_file, 'w') as output_file:
         json.dump(genotype_data, output_file, indent=4)
     
+    
+def parse_genotype_file(genotype_file_path: str) -> Dict[str, str]:
+    genotype_dict = {}
+    with open(genotype_file_path, 'r') as genotype_file:
+        for line in genotype_file:
+            if line.startswith("#"):
+                continue
+            parts = line.split()
+            var_id = parts[2]
+            sample_genotype = parts[9]
+            genotype = sample_genotype.split(":")[0]
+            genotype_dict[var_id] = genotype
+    return genotype_dict
+
 
 if __name__ == '__main__':
     main()
