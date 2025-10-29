905231ed017f03da740c173fdfb45807cf68be61
Viktor Henmyr <viktor_henmyr@hotmail.com>
2025-10-29 12:04:50 +0100

    args, writes to out_vcf

--- bin/cftr_5T_TG.py
+++ bin/cftr_5T_TG.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from pysam import VariantFile, AlignmentFile
+import cmdvcf
 import argparse
 import itertools
 from pprint import pprint
 from collections import defaultdict
 
+SCORE_ADJUSTMENT = 25
 CENTER_7T = 117548632
 PADDING_7T = 10
 REF_T_REPS = 7
@@ -14,7 +16,7 @@ CENTER_11TG = 117548617
 PADDING_11TG = 20
 REF_TG_REPS = 11
 BAD_TG_COUNT = 13
-cftr_region = { 
+CFTR_REGION = { 
     "start": 117548601,
     "end" : 117548640,
     "ref": ['T', 'T', 'T', 'T', 'G', 'A', 'T', 'G', 'T', 'G', 'T', 'G', 'T', 'G', 'T', 'G',
@@ -24,7 +26,7 @@ cftr_region = {
 """
 Logic of script:
 0: check that there are any variants at all..
-1: read the bam-file for the CFTR intron 8 region defined in cftr_region
+1: read the bam-file for the CFTR intron 8 region defined in CFTR_REGION
     * collect reads containing the 7T region
     * collect reads containing the 11TG region
     * check if allele counts could be considered causative BAD_T_COUNT/BAD_TG_COUNTS
@@ -43,15 +45,17 @@ def main(args):
     bam = AlignmentFile(args.bam, "rb")
     
     variants = []
-    for var in vcf_object.fetch("7", cftr_region['start'], cftr_region["end"]):
+    variation_combo_str = None
+    for var in vcf_object.fetch("7", CFTR_REGION['start'], CFTR_REGION["end"]):
         variants.append({
             "pos": var.pos,
             "ref": var.ref,
             "alt": var.alts[0],
-            "id": f"{var.pos}_{var.ref}_{var.alts[0]}"
+            "id": f"7_{var.pos}_{var.ref}_{var.alts[0]}"
         })
     if len(variants) == 0:
-        print("no relevant variants detected")
+        print_vcf(vcf_object,args.out_vcf,variation_combo_str)
+        exit()
 
     print("Trying to find 5T variants")
     fragments_5t = get_overlapping_reads_from_bam(bam,CENTER_7T,PADDING_7T)
@@ -61,8 +65,8 @@ def main(args):
         count[max_count] = count[max_count] +1
     allele1,allele2 = find_one_or_two_alleles(count)
 
-    if BAD_T_COUNT in [allele1,allele2]:
-        variation_combo = combinatorics_to_find_T_causatives(variants,[allele1,allele2])
+    if allele1 <= BAD_T_COUNT or allele2 <= BAD_T_COUNT: 
+        variation_combo_str = combinatorics_to_find_T_causatives(variants,[allele1,allele2])
     else:
         print("Trying to find 13TG+ variants")
         if allele1 < BAD_TG_COUNT or allele2 < BAD_TG_COUNT:
@@ -73,8 +77,35 @@ def main(args):
             max_count = count_consecutive_tgs(fragment)
             count[max_count] = count[max_count] +1
         allele1,allele2 = find_one_or_two_alleles(count)
-        variation_combo = combinatorics_to_find_TG_causatives(variants,[allele1,allele2])
+        variation_combo_str = combinatorics_to_find_TG_causatives(variants,[allele1,allele2])
+    
+    print_vcf(vcf_object,args.out_vcf,variation_combo_str)
 
+
+def print_vcf(in_vcf,out_vcf,variation_combo_str):
+    """
+    """
+    ids_of_variants_to_modify = []
+    if variation_combo_str:
+        ids_of_variants_to_modify = variation_combo_str.split('+')
+    
+    vcf_out = VariantFile(out_vcf, "w", header=in_vcf.header)
+    vcf_out.close()
+    with open(out_vcf, "a") as vcf_out:
+        for var in in_vcf.fetch():
+            var_dict = cmdvcf.parse_variant(var,in_vcf.header)
+            var_id = f"{var_dict['CHROM']}_{var_dict['POS']}_{var_dict['REF']}_{var_dict['ALT']}"
+            if var_id in ids_of_variants_to_modify:
+                if var_dict['INFO'].get('RankScore'):
+                    family_id = str(var_dict['INFO']['RankScore'].split(":")[0])
+                    current_score = int(var_dict['INFO']['RankScore'].split(":")[1])
+                    adjusted_score = current_score + SCORE_ADJUSTMENT
+                    var_dict['INFO']['RankScore'] = f"{family_id}:{str(adjusted_score)}"
+                else:
+                    exit("Needs a scored VCF")
+            vcf_str = cmdvcf.vcf_string(var_dict,in_vcf.header)
+            vcf_out.write(vcf_str)
+        
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Adds variantcaller specific penalties to variants with only one variant caller")
@@ -174,7 +205,7 @@ def apply_variants(base_seq, variants):
         ref = var["ref"]
         alt = var["alt"]
         pos = var["pos"]
-        relative_position = pos - cftr_region['start']
+        relative_position = pos - CFTR_REGION['start']
 
         # Deletion
         if len(ref) > 1:
@@ -202,7 +233,7 @@ def combinatorics_to_find_T_causatives(variants, alleles):
     # Iterate over all variant combinations
     for r in range(1, len(variants) + 1):
         for combo in itertools.combinations(variants, r):
-            ref_motif_variant = apply_variants(cftr_region['ref'], combo)
+            ref_motif_variant = apply_variants(CFTR_REGION['ref'], combo)
             variant_ids = [v['id'] for v in combo]
             combo_label = "+".join(variant_ids)
 
@@ -216,7 +247,7 @@ def combinatorics_to_find_TG_causatives(variants,alleles):
     # Iterate over all variant combinations
     for r in range(1, len(variants) + 1):
         for combo in itertools.combinations(variants, r):
-            ref_motif_variant = apply_variants(cftr_region['ref'], combo)
+            ref_motif_variant = apply_variants(CFTR_REGION['ref'], combo)
             variant_ids = [v['id'] for v in combo]
             combo_label = "+".join(variant_ids)
             variant_change_TG_count = count_consecutive_tgs(''.join(ref_motif_variant))
