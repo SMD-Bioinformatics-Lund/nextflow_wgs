12c3308f60370e4471a4c06fcfe93515542210bc
Viktor Henmyr <viktor_henmyr@hotmail.com>
2026-01-14 14:24:12 +0100

    black formatting

--- bin/plot_cnvkit_genes.py
+++ bin/plot_cnvkit_genes.py
@@ -5,9 +5,10 @@ import gzip
 import matplotlib.pyplot as plt
 import argparse
 
+
 def main(args):
     gene = args.gene
-    
+
     gtf_file = args.gtf
     cns_file = args.cns
     cnr_file = args.cnr
@@ -16,8 +17,17 @@ def main(args):
     if args.output:
         out_file = args.output
 
-
-    plot_gene_cnv(gene,gtf_file,cnr_file,cns_file,flank=50_000,ymin=-5,ymax=5,out_png=out_file,vcf_file=args.scored_vcf)
+    plot_gene_cnv(
+        gene,
+        gtf_file,
+        cnr_file,
+        cns_file,
+        flank=50_000,
+        ymin=-5,
+        ymax=5,
+        out_png=out_file,
+        vcf_file=args.scored_vcf,
+    )
 
 
 def plot_gene_cnv(
@@ -29,7 +39,7 @@ def plot_gene_cnv(
     ymin=-5,
     ymax=5,
     out_png=None,
-    vcf_file=None
+    vcf_file=None,
 ):
     # Load GTF
     gtf = pd.read_csv(
@@ -38,11 +48,17 @@ def plot_gene_cnv(
         comment="#",
         header=None,
         names=[
-            "chrom", "source", "feature",
-            "start", "end", "score",
-            "strand", "frame", "attr"
+            "chrom",
+            "source",
+            "feature",
+            "start",
+            "end",
+            "score",
+            "strand",
+            "frame",
+            "attr",
         ],
-        low_memory=False
+        low_memory=False,
     )
 
     # Only get data for supplied gene
@@ -60,10 +76,10 @@ def plot_gene_cnv(
 
     # GTF is 1-based cnvkit bedlike is not, convert
     gene_start = gene_gtf["start"].min() - 1
-    gene_end   = gene_gtf["end"].max()
+    gene_end = gene_gtf["end"].max()
 
     region_start = max(0, gene_start - flank)
-    region_end   = gene_end + flank
+    region_end = gene_end + flank
 
     exons = gene_gtf[gene_gtf["feature"] == "exon"]
 
@@ -84,17 +100,17 @@ def plot_gene_cnv(
         chrom = chrom.replace("chr", "")
 
     cnr_r = cnr[
-        (cnr["chromosome"] == chrom) &
-        (cnr["start"] < region_end) &
-        (cnr["end"] > region_start)
+        (cnr["chromosome"] == chrom)
+        & (cnr["start"] < region_end)
+        & (cnr["end"] > region_start)
     ]
 
     cns_r = cns[
-        (cns["chromosome"] == chrom) &
-        (cns["start"] < region_end) &
-        (cns["end"] > region_start)
+        (cns["chromosome"] == chrom)
+        & (cns["start"] < region_end)
+        & (cns["end"] > region_start)
     ]
-    
+
     # Plot segments, probes and exons
     fig, ax = plt.subplots(figsize=(10, 4))
 
@@ -104,18 +120,12 @@ def plot_gene_cnv(
         cnr_r["log2"],
         s=10,
         alpha=0.5,
-        color="grey"
+        color="grey",
     )
 
     # called segments
     for _, seg in cns_r.iterrows():
-        ax.hlines(
-            seg["log2"],
-            seg["start"],
-            seg["end"],
-            linewidth=2,
-            color="firebrick"
-        )
+        ax.hlines(seg["log2"], seg["start"], seg["end"], linewidth=2, color="firebrick")
 
     # exons
     exon_y = ymin + 0.2
@@ -127,7 +137,7 @@ def plot_gene_cnv(
                 (exon["start"] - 1, exon_y),
                 exon["end"] - exon["start"],
                 exon_h,
-                color="steelblue"
+                color="steelblue",
             )
         )
 
@@ -139,27 +149,18 @@ def plot_gene_cnv(
         x2 = exons_sorted.iloc[i + 1]["start"] - 1
 
         ax.hlines(
-            y=exon_y + exon_h / 2,
-            xmin=x1,
-            xmax=x2,
-            color="steelblue",
-            linewidth=1
+            y=exon_y + exon_h / 2, xmin=x1, xmax=x2, color="steelblue", linewidth=1
         )
 
     # highlight gene
-    ax.axvspan(
-        gene_start,
-        gene_end,
-        color="steelblue",
-        alpha=0.1
-    )
+    ax.axvspan(gene_start, gene_end, color="steelblue", alpha=0.1)
 
-    sv_ymin = ymin   # bottom of CNV plot
-    sv_ymax = exon_y + exon_h + 0.2 # just below exons
+    sv_ymin = ymin  # bottom of CNV plot
+    sv_ymax = exon_y + exon_h + 0.2  # just below exons
 
     if vcf_file:
         sv_df = parse_vcf(vcf_file, chrom, region_start, region_end)
-        
+
         for _, sv in sv_df.iterrows():
             ax.axvspan(
                 sv["start"],
@@ -167,7 +168,7 @@ def plot_gene_cnv(
                 ymin=(sv_ymin - ymin) / (ymax - ymin),
                 ymax=(sv_ymax - ymin) / (ymax - ymin),
                 color="orange",
-                alpha=0.3
+                alpha=0.3,
             )
 
             # vertical text
@@ -177,7 +178,7 @@ def plot_gene_cnv(
                 f'{sv["SVTYPE"]}:{sv["RankScore"]}',
                 fontsize=7,
                 ha="center",
-                va="top"
+                va="top",
             )
 
     ax.set_xlim(region_start, region_end)
@@ -186,7 +187,6 @@ def plot_gene_cnv(
     ax.set_xlabel(f"{chrom}:{region_start:,}-{region_end:,}")
     ax.set_title(gene)
 
-    plt.tight_layout()
     plt.tight_layout()
 
     if out_png:
@@ -224,61 +224,58 @@ def parse_vcf(vcf_file, chrom, region_start, region_end):
             # end is not VCF standard but we use it, fallback
             end = int(info_dict.get("END", pos))
 
-            svs.append({
-                "start": pos,
-                "end": end,
-                "SVTYPE": svtype,
-                "RankScore": rankscore
-            })
+            svs.append(
+                {"start": pos, "end": end, "SVTYPE": svtype, "RankScore": rankscore}
+            )
 
     return pd.DataFrame(svs)
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Plots gene coverage for provided gene name for a given CNVkit called sample")
+    parser = argparse.ArgumentParser(
+        description="Plots gene coverage for provided gene name for a given CNVkit called sample"
+    )
 
     parser.add_argument(
         "--cnr",
         "-r",
         type=str,
         required=True,
-        help="Path to CNVkit regions file (.cnr)"
+        help="Path to CNVkit regions file (.cnr)",
     )
     parser.add_argument(
         "--cns",
         "-s",
         type=str,
         required=True,
-        help="Path to CNVkit segments file (.cns)"
+        help="Path to CNVkit segments file (.cns)",
     )
     parser.add_argument(
         "--gtf",
         "-d",
         type=str,
         required=True,
-        help="path to gtf file, preferably mane transcripts only"
+        help="path to gtf file, preferably mane transcripts only",
     )
     parser.add_argument(
         "--output",
         "-o",
         type=str,
-        help="name of output png, if not provided output will be gene_name.png"
+        help="name of output png, if not provided output will be gene_name.png",
     )
     parser.add_argument(
-        "--gene",
-        "-g",
-        type=str,
-        required=True,  
-        help="HGNC symbol name"
+        "--gene", "-g", type=str, required=True, help="HGNC symbol name"
     )
     parser.add_argument(
         "--scored_vcf",
         "-v",
         type=str,
-        help="Optionally add scored vcf to highlight calls"
+        help="Optionally add scored vcf to highlight calls",
     )
     args = parser.parse_args()
-    
+
     return args
+
 
 if __name__ == "__main__":
     args = parse_arguments()
