8d949a7b518d955957cfffbd17f9963cd8aa5bbb
Viktor Henmyr <viktor_henmyr@hotmail.com>
2026-04-09 08:31:59 +0200

    per case instead of per group

--- bin/peddy2cdm.py
+++ bin/peddy2cdm.py
@@ -2,17 +2,19 @@
 import csv
 import json
 
-def load_sex_check(file_path):
-    sex_data = {}
-
+def load_sex_check(file_path, sample):
+    sex_ok = True
+    found = False
     with open(file_path, newline='') as f:
         reader = csv.DictReader(f)
         for row in reader:
             sample_id = row["sample_id"]
-            sex_ok = row["error"].lower() == "false"
-            sex_data[sample_id] = sex_ok
-
-    return sex_data
+            if sample_id == sample:
+                found = True
+                sex_ok = row["error"].lower() == "false"
+    if not found:
+        exit(f"{sample} is not in pedigree")
+    return sex_ok
 
 
 def load_ped_check(file_path):
@@ -24,19 +26,17 @@ def load_ped_check(file_path):
     return ped_rows
 
 
-def evaluate_kinship(ped_rows, proband, father, mother):
+def evaluate_kinship(ped_rows, sample_id):
     """
-    return mother father errors
+    returns all matchings to other samples in trio
     """
     rel_status = {}
 
     for row in ped_rows:
-        if row['sample_a'] == proband and row['sample_b'] == mother:
-            rel_status['mother'] = is_correct(row)
-            rel_status['mother_id'] = mother
-        if row['sample_a'] == proband and row['sample_b'] == father:
-            rel_status['father'] = is_correct(row)
-            rel_status['father_id'] = father
+        if row['sample_a'] == sample_id:
+            rel_status[row['sample_b']] = is_correct(row)
+        if row['sample_b'] == sample_id:
+            rel_status[row['sample_a']] = is_correct(row)
 
     return rel_status
 
@@ -46,49 +46,38 @@ def is_correct(row):
     return is_correct
     
 
-def build_per_sample_outputs(sex_data, ped_rows, proband, father, mother):
+def build_per_sample_outputs(sex_ok, ped_rows, sample):
     is_trio = len(ped_rows) > 0
 
-    results = {}
+    if is_trio:
+        rel_status = evaluate_kinship(ped_rows, sample)
+
+    result = {
+        "trio": is_trio,
+        "sex": sex_ok
+    }
 
     if is_trio:
-        rel_status = evaluate_kinship(ped_rows, proband, father, mother)
+        result['kinship'] = rel_status
 
-    for sample_id, sex_ok in sex_data.items():
-        result = {
-            "trio": is_trio,
-            "sex": sex_ok
-        }
-
-        if is_trio:
-            if sample_id == proband:
-                result["kinship"] = rel_status
-            elif sample_id in (father, mother):
-                role = "father" if sample_id == father else "mother"
-                result["kinship"] = {"proband": rel_status.get(role, False), "proband_id" : proband}
-
-        results[sample_id] = result
-
-    return results
+    return result
 
 
-def write_individual_jsons(results):
-
-    for sample_id, data in results.items():
-        filename = f"{sample_id}.json"
-        with open(filename, "w") as f:
-            json.dump(data, f, indent=2)
+def write_individual_jsons(results, sample):
 
 
-def main(ped_file, sex_file, proband, father, mother):
-    sex_data = load_sex_check(sex_file)
+    filename = f"{sample}.json"
+    with open(filename, "w") as f:
+        json.dump(results, f, indent=2)
+
+
+def main(ped_file, sex_file, sample):
+    sex_ok = load_sex_check(sex_file,sample)
     ped_rows = load_ped_check(ped_file)
 
-    results = build_per_sample_outputs(sex_data, ped_rows, proband, father, mother)
+    result = build_per_sample_outputs(sex_ok, ped_rows, sample)
 
-    write_individual_jsons(results)
-
-    print(f"Wrote {len(results)} JSON files")
+    write_individual_jsons(result, sample)
 
 
 if __name__ == "__main__":
@@ -97,10 +86,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process PED and SEX check files")
     parser.add_argument("--ped", required=True, help="id.ped_check.csv")
     parser.add_argument("--sex", required=True, help="id.sex_check.csv")
-    parser.add_argument("--proband", required=True, help="proband-id")
-    parser.add_argument("--mother", default=None, help="mother-id")
-    parser.add_argument("--father", default=None, help="father-id")
+    parser.add_argument("--sample", required=True, help="id of sample")
+
 
     args = parser.parse_args()
 
-    main(args.ped, args.sex, args.proband, args.father, args.mother)
+    main(args.ped, args.sex, args.sample)
