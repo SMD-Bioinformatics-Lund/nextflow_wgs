4198722150ac3ab3b843f341b9c1c1d9116786aa
Viktor Henmyr <viktor_henmyr@hotmail.com>
2026-04-13 09:01:40 +0200

    black

--- bin/peddy2cdm.py
+++ bin/peddy2cdm.py
@@ -4,10 +4,11 @@ import json
 import argparse
 from datetime import datetime, timezone
 
+
 def load_sex_check(file_path, sample):
     sex_ok = True
     found = False
-    with open(file_path, newline='') as f:
+    with open(file_path, newline="") as f:
         reader = csv.DictReader(f)
         for row in reader:
             sample_id = row["sample_id"]
@@ -22,7 +23,7 @@ def load_sex_check(file_path, sample):
 
 def load_ped_check(file_path):
     ped_rows = []
-    with open(file_path, newline='') as f:
+    with open(file_path, newline="") as f:
         reader = csv.DictReader(f)
         for row in reader:
             ped_rows.append(row)
@@ -36,27 +37,28 @@ def evaluate_kinship(ped_rows, sample_id, samples_dict):
     rel_status = {}
 
     for row in ped_rows:
-        if row['sample_a'] == sample_id:
-            other = row['sample_b']
+        if row["sample_a"] == sample_id:
+            other = row["sample_b"]
             rel_status[other] = {
                 "is_correct": is_correct(row),
-                "sequencing_run": samples_dict.get(other)
+                "sequencing_run": samples_dict.get(other),
             }
 
-        if row['sample_b'] == sample_id:
-            other = row['sample_a']
+        if row["sample_b"] == sample_id:
+            other = row["sample_a"]
             rel_status[other] = {
                 "is_correct": is_correct(row),
-                "sequencing_run": samples_dict.get(other)
+                "sequencing_run": samples_dict.get(other),
             }
 
     return rel_status
+
 
 def is_correct(row):
     parent_error = row["parent_error"].lower() == "true"
     is_correct = not parent_error
     return is_correct
-    
+
 
 def build_per_sample_outputs(sex_ok, ped_sex, ped_rows, sample, samples_dict):
     is_trio = len(ped_rows) > 0
@@ -68,32 +70,35 @@ def build_per_sample_outputs(sex_ok, ped_sex, ped_rows, sample, samples_dict):
 
     sex_check = {
         "is_correct_sex": sex_ok,
-        "pedigree_sex" : ped_sex,
+        "pedigree_sex": ped_sex,
     }
 
     result["sex_check"] = sex_check
 
     if is_trio:
-        result['kinship'] = rel_status
+        result["kinship"] = rel_status
 
-    result['analysis_date'] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    result["analysis_date"] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
 
     return result
 
 
 def write_individual_jsons(results, output):
 
-
     filename = output
     with open(filename, "w") as f:
         json.dump(results, f, indent=2)
 
-def write_cdm_load(sample,cdmassay,output_file,samples_dict,results_dir):
+
+def write_cdm_load(sample, cdmassay, output_file, samples_dict, results_dir):
 
     seqrun = samples_dict.get(sample)
     filename = f"{sample}.peddy2cdm"
     with open(filename, "w") as f:
-        f.write(f"--sequencing-run {seqrun} --assay {cdmassay} --sample-id {sample} --peddy {results_dir}/{output_file}")
+        f.write(
+            f"--sequencing-run {seqrun} --assay {cdmassay} --sample-id {sample} --peddy {results_dir}/{output_file}"
+        )
+
 
 def parse_samples(sample_arg):
     """
@@ -116,7 +121,8 @@ def parse_samples(sample_arg):
 
     return samples
 
-def main(ped_file, sex_file, sample_arg, cdmassay,results_dir):
+
+def main(ped_file, sex_file, sample_arg, cdmassay, results_dir):
     ped_rows = load_ped_check(ped_file)
 
     samples_dict = parse_samples(sample_arg)
@@ -125,23 +131,17 @@ def main(ped_file, sex_file, sample_arg, cdmassay,results_dir):
         sex_ok, ped_sex = load_sex_check(sex_file, sample)
 
         result = build_per_sample_outputs(
-            sex_ok,
-            ped_sex,
-            ped_rows,
-            sample,
-            samples_dict
+            sex_ok, ped_sex, ped_rows, sample, samples_dict
         )
 
-
         output_file = f"{sample}_peddy.json"
-        
+
         write_individual_jsons(result, output_file)
-        write_cdm_load(sample,cdmassay,output_file,samples_dict,results_dir)
-        
+        write_cdm_load(sample, cdmassay, output_file, samples_dict, results_dir)
 
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser(description="Process PED and SEX check files")
     parser.add_argument("--ped", required=True, help="id.ped_check.csv")
     parser.add_argument("--sex", required=True, help="id.sex_check.csv")
